8ce89c9d7a049e673f5e8a9b6c956747d7650d89
isabela-souzahara <isabela.meninashara@gmail.com>
2025-11-09 06:42:39 -0600

    Added search for favorites, fixes to te page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,43 +1,59 @@
 import ModelCard from "@/components/ModelCard";
+import Search from "@/components/Search";
+import { useMemo, useState } from "react";
 
 const favoritesList = [
   { name: "Mewo", image: "", favorite: true, handleLike: () => {} },
   { name: "Ball", image: "", favorite: true, handleLike: () => {} },
   { name: "Car", image: "", favorite: true, handleLike: () => {} },
-  { name: "Apple", image: "", favorite: true, handleLike: () => {}},
-];  
+  { name: "Apple", image: "", favorite: true, handleLike: () => {} },
+];
 
-const sortBy = "Alphabetical";
+function sortFavorites(sortMethod: string) {
+  const displayList = [...favoritesList];
 
-function sortFavorites() {
-  var displayList = [...favoritesList];
-
-  if (sortBy == "Alphabetical") {
-    displayList = displayList.sort((first, second) => first.name.localeCompare(second.name));
+  if (sortMethod === "Alphabetical") {
+    return displayList.sort((first, second) => first.name.localeCompare(second.name));
   }
 
   return displayList;
 }
 
-const sortedFavorites = sortFavorites();
-
 export default function Favorites() {
+  const [sortBy, setSortBy] = useState<string>("Alphabetical");
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const sortedFavorites = useMemo(() => {
+    var list = sortFavorites(sortBy);
+    if (searchQuery)  {
+      const query = searchQuery.trim().toLowerCase();
+      list = list.filter((item) => item.name.toLowerCase().includes(query));
+    }
+    return list
+  }, [sortBy, searchQuery]);
+
   return (
-    <div className="w-full h-full text-white bg-black">
+    <div className="w-full h-full text-black bg-white">
       <div className="w-full p-3 border border-zinc-600 shadow-md">
 
         <div className="inline-block text-3xl py-3 font-bold">Favorites</div>
-
         <div className="inline-block ml-10">Sort by:
-          <select className="ml-2 p-2 border bg-black border-zinc-600 rounded ">
-            <option value="date">Date Added</option>
+          <select className="ml-2 p-2 border bg-white border-zinc-600 rounded" onChange={(e) => setSortBy(e.target.value)}>
+            <option value="Date">Date Added</option>
             <option value="Alphabetical">Alphabetical</option>
           </select>
+        </div>
+        <div className="float-right">
+          <Search 
+            value={searchQuery} 
+            onChange={setSearchQuery} 
+            placeholder="Search favorites" 
+          />
         </div>
 
       </div>
 
-      <div className="w-full aspect-3/2 grid grid-cols-3 gap-4 py-3 px-4">
+      <div className="w-full grid grid-cols-3 gap-4 py-5 px-4">
           {sortedFavorites.map((f) => (
             <ModelCard
               name={f.name}
