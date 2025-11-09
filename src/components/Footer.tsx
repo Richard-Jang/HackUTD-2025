79f4f46c03f5c440b02c8a2c40af81a3d5d47539
Richard Jang <jang.richard7017@gmail.com>
2025-11-08 22:36:42 -0600

    created footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return <div className="relative w-full h-32 bg-rose-600 flex flex-col items-center justify-center p-4">
+        <div className="w-full flex items-center gap-5">
+            <div className="grow h-0.5 border border-white"></div>
+            <div className="text-xl">Toyota DreamFind</div>
+            <div className="grow h-0.5 border border-white"></div>
+        </div>
+        <div className="absolute bottom-4 text-xs">© HackUTD 2025</div>
+    </div>
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,7 @@ export default function Home() {
 
   return (
     <div className="w-full h-full bg-black p-8">
-        <div className="w-1/3">
-            <ModelCard
-                name="Model Name"
-                image=""
-                favorite={false}
-                handleLike={() => {}}
-            />
-        </div>
+        
     </div>
   )
 }
