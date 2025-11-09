a39c1909e3b838ddde8edd3279be99b3b47d7e6e
Richard Jang <jang.richard7017@gmail.com>
2025-11-08 22:11:50 -0600

    created modelcard

---
+++ src/components/ModelCard.tsx
@@ -0,0 +1,43 @@
+import { FaHeart, FaRegHeart } from "react-icons/fa6";
+
+export default function ModelCard({
+    name,
+    image,
+    favorite,
+    handleLike,
+}: {
+    name: string;
+    image: string;
+    favorite: boolean;
+    handleLike: () => void;
+}) {
+
+    return <div className="flex flex-col rounded-lg border border-zinc-600 aspect-3/2">
+        <div className="relative w-full grow">
+            <div className="w-full h-full">
+                <img
+                    className="w-full h-full rounded-t-lg"
+                    src={image ? image : "https://placehold.co/600x400"}
+                />
+            </div>
+            {favorite ? (
+                <div
+                    className="p-3 absolute right-0 bottom-0 text-rose-600 hover:scale-125 transition-all text-xl"
+                    onClick={handleLike}
+                >
+                    <FaHeart />
+                </div>
+            ) : (
+                <div
+                    className="p-3 absolute right-0 bottom-0 text-rose-600 hover:scale-125 transition-all text-xl"
+                    onClick={handleLike}
+                >
+                    <FaRegHeart />
+                </div>
+            )}
+        </div>
+        <div className="flex items-center justify-start border-t-1 border-zinc-600 px-4 py-3">
+            {name}
+        </div>
+    </div>
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
+import ModelCard from "@/components/ModelCard";
+
 export default function Home() {
 
   return (
-    <div className="w-full h-full bg-black">
-        HAHHHHHAHAHSHADFAHSFHASHFHASFHAJKSFKASJFSAFBSJKAFNHSKDF
+    <div className="w-full h-full bg-black p-8">
+        <div className="w-1/3">
+            <ModelCard
+                name="Model Name"
+                image=""
+                favorite={false}
+                handleLike={() => {}}
+            />
+        </div>
     </div>
   )
 }
