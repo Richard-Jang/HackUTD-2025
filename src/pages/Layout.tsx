3b57148c7ca7bfb195fcb37e121f063f38e1ea6d
Richard Jang <jang.richard7017@gmail.com>
2025-11-09 02:17:21 -0600

    added useLocalStorage and carousel

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return <div
-        className="max-w-screen max-h-screen w-screen h-screen overflow-y-auto overflow-x-hidden"
+        className="max-w-screen max-h-screen w-screen h-screen overflow-y-auto overflow-x-hidden bg-white text-black"
     >
         <Outlet />
         <Footer />
